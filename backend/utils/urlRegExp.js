3b792682d29e3d403bd2c12ebc0559e24363bd90
Majukin <99135527+Majukin@users.noreply.github.com>
2023-04-12 21:33:01 +0300

    Delete backend directory

--- backend/utils/urlRegExp.js
+++
@@ -1,3 +0,0 @@
-const reg = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
-
-module.exports = { reg };
